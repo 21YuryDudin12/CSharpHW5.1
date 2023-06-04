78e922ea8363daf9d4e2ed4de69327b50f941fba
Yury Dudin <cepob33@gmail.com>
2023-06-04 12:56:39 +0500

    added description

--- ex34/Program.cs
+++ ex34/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] NewArray (int size, int minValue, int maxValue)
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] NewArray (int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
     for (int i = 0; i<size; i++)
